b2fc6678237552733488430a0dd043420176a7f2
Ashley Roarick <ahorseroar@gmail.com>
2021-02-16 10:07:33 -0600

    added skills row

--- src/views/home.js
+++ src/views/home.js
@@ -39,34 +39,80 @@ function Home() {
             Whether drawing in my notebooks or designing in Photoshop, creating
             is an important part of who I am.
           </p>
-          <p className="specialties-skills">Design Skills</p>
-          <ul className="specialties-skills-list">
-            <li>UX</li>
-            <li>UI</li>
-            <li>Responsive Design</li>
-          </ul>
+          <Row>
+            <Col>
+              <p className="specialties-skills">Design Skills</p>
+              <ul className="specialties-skills-list">
+                <li>UX</li>
+                <li>UI</li>
+                <li>Responsive Design</li>
+              </ul>
+            </Col>
+            <Col>
+              <p className="specialties-skills">Tools</p>
+              <ul className="specialties-skills-list">
+                <li>Photoshop</li>
+                <li>Balsamiq</li>
+                <li>Pen & Paper</li>
+              </ul>
+            </Col>
+          </Row>
         </Col>
+        {/* Frontend Col */}
         <Col>
           <h1 className="specialties-title">Front-end</h1>
           <p className="specialties-description">
             I like to design and code things from scratch. I enjoy bringing my
             ideas to life in the browser.
           </p>
-          <p className="specialties-skills">Design Skills</p>
-          <ul className="specialties-skills-list">
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>SASS</li>
-            <li>Javascript</li>
-            <li>ReactJS</li>
-          </ul>
+          <Row>
+            <Col>
+              <p className="specialties-skills">Design Skills</p>
+              <ul className="specialties-skills-list">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>SASS</li>
+                <li>Javascript</li>
+                <li>ReactJS</li>
+              </ul>
+            </Col>
+            <Col>
+              <p className="specialties-skills">Tools</p>
+              <ul className="specialties-skills-list">
+                <li>Bootstrap 3/4</li>
+                <li>Git/Github/Gitlab</li>
+                <li>Git Bash</li>
+              </ul>
+            </Col>
+          </Row>
         </Col>
+        {/* Backend Col */}
         <Col>
           <h1 className="specialties-title">Back-end</h1>
           <p className="specialties-description">
             I have a passion for developing applications or improving existing
-            ones. I also enjoy being up to date with all the new technologies.
+            ones.
           </p>
+          <Row>
+            <Col>
+              <p className="specialties-skills">Skills</p>
+              <ul className="specialties-skills-list">
+                <li>Express</li>
+                <li>Sequelize</li>
+                <li>NodeJS</li>
+                <li>Java</li>
+                <li>Python</li>
+              </ul>
+            </Col>
+            <Col>
+              <p className="specialties-skills">Tools</p>
+              <ul className="specialties-skills-list">
+                <li>MySQL</li>
+                <li>SendGrid</li>
+                <li>Postman</li>
+              </ul>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </>
